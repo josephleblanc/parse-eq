13c22cbe2b679fb7e3194bea0cd81577c5e63a8d
Joseph LeBlanc <joseph.t.leblanc@protonmail.com>
2024-10-08 09:10:05 -0700

    add more tests

--- tests/tree_test.rs
+++ tests/tree_test.rs
@@ -94,3 +94,95 @@ fn tree_parens() {
     let tree: Tree = Tree::new_pre_from_in(in_order);
     assert_eq!(tree, check_tree);
 }
+
+#[test]
+fn tree_parens_many() {
+    use binary_tree_ds::TreeNode;
+    use parse_eq::lexer::Lexer;
+    use parse_eq::token::Operator::*;
+    use parse_eq::token::Token::*;
+    use parse_eq::tree::Tree;
+
+    // 2 * ( 5 * 3 + 4 / ( 1 + 6 ) )
+    let one = Rc::new(RefCell::new(TreeNode::new(Number(1.0), None, None)));
+    let two = Rc::new(RefCell::new(TreeNode::new(Number(2.0), None, None)));
+    let three = Rc::new(RefCell::new(TreeNode::new(Number(3.0), None, None)));
+    let four = Rc::new(RefCell::new(TreeNode::new(Number(4.0), None, None)));
+    let five = Rc::new(RefCell::new(TreeNode::new(Number(5.0), None, None)));
+    let six = Rc::new(RefCell::new(TreeNode::new(Number(6.0), None, None)));
+
+    let lower_plus = Rc::new(RefCell::new(TreeNode::new(Op(Add), Some(one), Some(six))));
+    let division = Rc::new(RefCell::new(TreeNode::new(
+        Op(Divide),
+        Some(four),
+        Some(lower_plus),
+    )));
+    let lower_mult = Rc::new(RefCell::new(TreeNode::new(
+        Op(Multiply),
+        Some(five),
+        Some(three),
+    )));
+    let upper_plus = Rc::new(RefCell::new(TreeNode::new(
+        Op(Add),
+        Some(lower_mult),
+        Some(division),
+    )));
+    let upper_mult = Rc::new(RefCell::new(TreeNode::new(
+        Op(Multiply),
+        Some(two),
+        Some(upper_plus),
+    )));
+    let check_tree: Tree = Tree::new(upper_mult);
+
+    let lexer = Lexer::new_inorder("( ( 2 * ( 5 * 3 + 4 / ( 1 + 6 ) ) ) )").unwrap();
+    let in_order = lexer.list;
+
+    let tree: Tree = Tree::new_pre_from_in(in_order);
+    assert_eq!(tree, check_tree);
+}
+
+#[test]
+fn tree_parens_many_more() {
+    use binary_tree_ds::TreeNode;
+    use parse_eq::lexer::Lexer;
+    use parse_eq::token::Operator::*;
+    use parse_eq::token::Token::*;
+    use parse_eq::tree::Tree;
+
+    // 2 * ( 5 * 3 + 4 / ( 1 + 6 ) )
+    let one = Rc::new(RefCell::new(TreeNode::new(Number(1.0), None, None)));
+    let two = Rc::new(RefCell::new(TreeNode::new(Number(2.0), None, None)));
+    let three = Rc::new(RefCell::new(TreeNode::new(Number(3.0), None, None)));
+    let four = Rc::new(RefCell::new(TreeNode::new(Number(4.0), None, None)));
+    let five = Rc::new(RefCell::new(TreeNode::new(Number(5.0), None, None)));
+    let six = Rc::new(RefCell::new(TreeNode::new(Number(6.0), None, None)));
+
+    let lower_plus = Rc::new(RefCell::new(TreeNode::new(Op(Add), Some(one), Some(six))));
+    let division = Rc::new(RefCell::new(TreeNode::new(
+        Op(Divide),
+        Some(four),
+        Some(lower_plus),
+    )));
+    let lower_mult = Rc::new(RefCell::new(TreeNode::new(
+        Op(Multiply),
+        Some(five),
+        Some(three),
+    )));
+    let upper_plus = Rc::new(RefCell::new(TreeNode::new(
+        Op(Add),
+        Some(lower_mult),
+        Some(division),
+    )));
+    let upper_mult = Rc::new(RefCell::new(TreeNode::new(
+        Op(Multiply),
+        Some(two),
+        Some(upper_plus),
+    )));
+    let check_tree: Tree = Tree::new(upper_mult);
+
+    let lexer = Lexer::new_inorder("( ( 2 * ( ( 5 * 3 ) + 4 / ( ( 1 ) + ( 6 ) ) ) ) )").unwrap();
+    let in_order = lexer.list;
+
+    let tree: Tree = Tree::new_pre_from_in(in_order);
+    assert_eq!(tree, check_tree);
+}
